f6e75a164b4f24abbcf8a7d7a444d72a24dcfd0f
Jan Nahody <jan.nahody@gmail.com>
2017-07-06 21:41:33 +0200

    Add date scaling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-document.body.innerHTML = `<h1>${d3.version}</h1>`;
+/*
+ * Date Range functions
+ */
+
+var currentTime = new Date()
+
+var timeScale = d3.scaleTime()
+    .domain([new Date(2016, 0, 1), currentTime])
+    .range([0, 100]);
+
+console.log(timeScale(new Date(2017,4,15)));
+console.log(timeScale(currentTime));
+console.log(timeScale.invert(50));
